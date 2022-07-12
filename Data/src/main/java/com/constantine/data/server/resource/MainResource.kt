2843ea797a9c2bea4db2b0b83ba5893d14098668
Constantine Kevin <constantine@siliconbear.com>
2022-07-12 02:17:27 +0200

    chore(dialer): add index resource implemetation

--- Data/src/main/java/com/constantine/data/server/resource/MainResource.kt
+++ Data/src/main/java/com/constantine/data/server/resource/MainResource.kt
@@ -2,12 +2,39 @@ package com.constantine.data.server.resource
 
 import com.constantine.data.config.scope.Endpoint
 import com.constantine.data.content.Resource
+import com.constantine.domain.usecase.InstallTimestampUsecase
+import com.google.gson.Gson
 import fi.iki.elonen.NanoHTTPD
+import java.util.Date
 import javax.inject.Inject
 
-class MainResource @Inject constructor() : Resource() {
+class MainResource @Inject constructor(
+    private val installTimestampUsecase: InstallTimestampUsecase
+) : Resource() {
     @Endpoint(method = NanoHTTPD.Method.GET, value = "/")
     fun index(session: NanoHTTPD.IHTTPSession): NanoHTTPD.Response {
-        return NanoHTTPD.newFixedLengthResponse("Hello, world")
+        val host = session.headers["host"] ?: ""
+        val date = Date(installTimestampUsecase.timestamp())
+        val response = mapOf(
+            Pair("start", date),
+            Pair(
+                "services",
+                listOf<Map<String, Any>>(
+                    mapOf(
+                        Pair("name", "status"),
+                        Pair("uri", "http://${host.trim('/')}/services/status")
+                    ),
+                    mapOf(
+                        Pair("name", "log"),
+                        Pair("uri", "http://${host.trim('/')}/services/logs")
+                    )
+                )
+            ),
+        )
+        return NanoHTTPD.newFixedLengthResponse(
+            NanoHTTPD.Response.Status.OK,
+            jsonMimeType,
+            Gson().toJson(response)
+        )
     }
 }
